37b26680c9fb2ccfb5bf42221fddef376607683f
sattamap <sattamap@gmail.com>
2023-12-21 14:50:13 +0600

    Created Banner component and added to the Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Banner from "../../components/Banner";
 
 const Home = () => {
     return (
         <div>
             <h2>This is my Home</h2>
+            <Banner></Banner>
         </div>
     );
 };
