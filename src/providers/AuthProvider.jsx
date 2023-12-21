8656af641926fa75ddb386aafe92f92adf3b349a
sattamap <sattamap@gmail.com>
2023-12-21 18:19:12 +0600

    feat: Add user authentication functions (createUser, signIn, googleSignIn, logOut)

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,21 +1,60 @@
 import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const createUser = (name, email, password, photoURL) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Add the user's first name and last name to their profile
+            const user = userCredential.user;
+            return updateProfile(user, {
+              displayName: `${name}`,
+              photoURL: photoURL || "" ,
+            });
+          })
+          .catch((error) => {
+            console.error("Error creating user:", error);
+            throw error; // Propagate the error to the caller
+          });
+      };
 
+      
+    const signIn = (email,password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    const googleSignIn =()=>{
+      return signInWithPopup(auth, googleProvider)
+    }
+
+
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
     
     const authInfo = {
         user,
         setUser,
+        createUser,
+        signIn,
+        googleSignIn,
+        logOut,
+        loading,
+        setLoading,
   
     };
     return (
