55704d84552677ebb321bf4445f2071fafdcf035
sattamap <sattamap@gmail.com>
2023-12-21 18:11:31 +0600

    init: Set up AuthProvider for user authentication

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    
+    const authInfo = {
+        user,
+        setUser,
+  
+    };
+    return (
+        <AuthContext.Provider value={authInfo}>
+        {children}
+    </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
