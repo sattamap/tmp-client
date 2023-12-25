374427d0bb75323a1e449120ef87aae01470fc46
sattamap <sattamap@gmail.com>
2023-12-25 16:52:00 +0600

    Install typewritter-effect and AOS and implement their effect on Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,21 +1,38 @@
 import { Link } from "react-router-dom";
-
+import tmpsBanner from "../assets/tmps_banner.jpg";
+import Typewriter from 'typewriter-effect';
 
 const Banner = () => {
-    return (
-        <div>
-            <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <Link to="/login"><button className="btn btn-primary">Let Explore</button></Link>
-    </div>
-  </div>
-</div>
+  return (
+    <div>
+      <div className="hero min-h-screen" style={{ backgroundImage: `url(${tmpsBanner})` }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+          <div data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000"><h1 className="mb-5 text-5xl font-bold">Collaborative <br /> Task Management Platform</h1></div>
+            
+            <p className="mb-5">
+            Elevate collaboration with effortless task synchronization. <br />
+            
+            <Typewriter
+                               options={{
+                                   strings: ['Simplify your workflow and stay organized.'],
+                                   autoStart: true,
+                                   loop: true,
+                               }}
+                           />
+            
+            </p>
+            <Link to="/login">
+              <button className="btn btn-primary">Get Started</button>
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,91 +2,126 @@ import { Link } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import { FaUserAlt } from "react-icons/fa";
 
-
 const Navbar = () => {
-    const {user,logOut} = useAuth();
+  const { user, logOut } = useAuth();
 
-    console.log("user is:",user);
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => console.log(error));
+  };
 
-    const handleLogOut = () => {
-        logOut()
-          .then(() => { })
-          .catch(error => console.log(error))
-      }
-
-    return (
-        <div className="max-w-full">
-            <div className="navbar fixed z-10 bg-slate-400 px-20 py-6">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <Link to="/"><li><a>Home</a></li></Link>
-        <Link to="/about"><li><a>About Us</a></li></Link>
-        <Link to="/dashboard"><li><a>Task Board</a></li></Link>
-       
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-    <Link to="/"><li><a>Home</a></li></Link>
-        <Link to="/about"><li><a>About Us</a></li></Link>
-        <Link to="/dashboard"><li><a>Task Board</a></li></Link>
-       
-    </ul>
-  </div>
-  <div className="navbar-end">
-  
-  
-  
-  {user ? (
-// If the user is logged in, show their profile picture, first name, and a logout button
-<div className="nav-item flex">
-{user.photoURL ? (
-<img
-src={user.photoURL}
-alt={`${user.displayName}'s Profile`}
-className="btn btn-ghost btn-circle avatar"
-/>
-) : (
-<FaUserAlt className="btn btn-ghost btn-circle avatar" />
-)}
-<span className="nav-link mx-2">
-Welcome, {user.displayName?.split(" ")[0]}!
-</span>
-<button className="btn bg-emerald-800 mx-2" onClick={handleLogOut}>
-Logout
-</button> 
-
-</div>
-) : (
-// If the user is not logged in, show the login button
-
-
-<div className="nav-item flex">
-<label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-<div>
-<FaUserAlt></FaUserAlt>
-</div>
-</label>
-
-<Link to="/login" className="btn btn-accent">
-Login
-</Link>
-
-</div>
-
-)}
-
-</div>
-</div>
-
+  return (
+    <div className="max-w-full">
+      <div className="navbar fixed z-10 bg-gray-800 px-20 py-6">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost lg:hidden"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-700 rounded-box w-52"
+            >
+              <Link to="/">
+                <li>
+                  <a className="text-white">Home</a>
+                </li>
+              </Link>
+              <Link to="/about">
+                <li>
+                  <a className="text-white">About Us</a>
+                </li>
+              </Link>
+              <Link to="/dashboard">
+                <li>
+                  <a className="text-white">Task Board</a>
+                </li>
+              </Link>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl text-white">daisyUI</a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <Link to="/">
+              <li>
+                <a className="text-white">Home</a>
+              </li>
+            </Link>
+            <Link to="/about">
+              <li>
+                <a className="text-white">About Us</a>
+              </li>
+            </Link>
+            <Link to="/dashboard">
+              <li>
+                <a className="text-white">Task Board</a>
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          {user ? (
+            <div className="nav-item flex items-center">
+              {user.photoURL ? (
+                <img
+                  src={user.photoURL}
+                  alt={`${user.displayName}'s Profile`}
+                  className="btn btn-ghost btn-circle avatar"
+                />
+              ) : (
+                <FaUserAlt className="btn btn-ghost btn-circle avatar" />
+              )}
+              <span className="nav-link mx-2 text-white">
+                Welcome, {user.displayName?.split(" ")[0]}!
+              </span>
+              <button
+                className="btn bg-emerald-800 mx-2"
+                onClick={handleLogOut}
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <div className="nav-item flex items-center">
+              <label
+                tabIndex={0}
+                className="btn btn-ghost btn-circle avatar text-white"
+              >
+                <div>
+                  <FaUserAlt />
+                </div>
+              </label>
+              <Link
+                to="/login"
+                className="btn btn-accent bg-emerald-800 ml-2"
+              >
+                Login
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
